12e1b42594a085ceb3402c7751c01c9967ed71c6
Emil Raczkiewicz <emil.raczkiewicz@gmail.com>
2025-04-09 18:29:24 +0200

    commit

--- new/settings.py
+++ new/settings.py
@@ -139,7 +139,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # STATIC_ROOT = BASE_DIR / "staticfiles"
